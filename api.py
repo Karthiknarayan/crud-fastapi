21f26e838fe2b1f4abd4b2fb88da0e8c00253967
Karthiknarayan <karthikn@vectrous.com>
2025-09-12 14:24:27 +0530

    added the post object to add the value in the dummydatabase

--- api.py
+++ api.py
@@ -5,12 +5,19 @@ dummydatabase={
 2:{"csk"},
 3:{"mi"}
 }
+#returns all the object in the dummydatabase
 @app.get('/')
-def returnitems():
+def returnTeams():
     return dummydatabase
 
-
-    
+ #returns the object in the dummydatabase based on id
 @app.get("/{id}")
-def getitembyid(id : int    ):
+def getTeamById(id : int):
     return dummydatabase[id]
+
+#add the value in the dummydatabase
+@app.post("/additem")
+def addTeam(team:str):
+    newid=len(dummydatabase.keys())+1
+    dummydatabase[newid]={team}
+    return dummydatabase
